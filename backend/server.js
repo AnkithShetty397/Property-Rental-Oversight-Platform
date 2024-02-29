80a9ee87a27a0808e5ea4b4242be838bbf71927a
AnkithShetty397 <ankithpshetty@gmail.com>
2024-02-29 22:19:21 +0530

    added file handling methods

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ const bodyParser    =require('body-parser')
 const reviewRoute   =require('./database/src/routes/reviewRoute')
 const houseRoute    =require('./database/src/routes/houseRoute')
 const searchRoute   =require('./database/src/routes/searchRoute')
+const profileRoute  =require('./database/src/routes/profileRoute')
 
 //connecting nodejs to mysql
 const mysqldb=mysql.createConnection({
@@ -51,3 +52,4 @@ app.listen(PORT,()=>{                               //start listening to the ser
 app.use('/api/review',reviewRoute)                  
 app.use('/api/house',houseRoute)
 app.use('/api/search',searchRoute)
+app.use('/api/profile',profileRoute)
